e3233a0784ea1f3237d35b14d7d8a0d7d8cc5aae
DoLim <limdongyoung0@gmail.com>
2020-02-23 06:55:24 +0000

    Studied graph io

---
+++ Everyday/Ch3/graph_io/graph_io_test.go
@@ -0,0 +1,86 @@
+package graph_io_test
+
+import (
+	"fmt"
+	"strings"
+	"bytes"
+	"io"
+	"testing"
+)
+
+func WriteTo(w io.Writer, adjList [][]int) error {
+	size := len(adjList)
+	if _, err := fmt.Fprintf(w, "%d", size); err != nil {
+		return err
+	}
+	for i := 0; i < size; i++ {
+		lsize := len(adjList[i])
+		if _, err := fmt.Fprintf(w, "\n%d", lsize); err != nil {
+			return err
+		}
+		for j := 0; j < lsize; j++ {
+			if _, err := fmt.Fprintf(w, " %d", adjList[i][j]); err != nil {
+				return err
+			}
+		}
+	}
+	if _, err := fmt.Fprintf(w, "\n"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadFrom(r io.Reader, adjList *[][]int) error {
+	var size int
+	if _, err := fmt.Fscanf(r, "%d", &size); err != nil {
+		return err
+	}
+	*adjList = make([][]int, size)
+	for i := 0; i < size; i++ {
+		var lsize int
+		if _, err := fmt.Fscanf(r, "\n%d", &lsize); err != nil {
+			return err
+		}
+		(*adjList)[i] = make([]int, lsize)
+		for j := 0; j < lsize; j++ {
+			if _, err := fmt.Fscanf(r, " %d", &(*adjList)[i][j]); err != nil {
+				return err
+			}
+		}
+	}
+	if _, err := fmt.Fscanf(r, "\n"); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func TestWriteTo(t *testing.T) {
+	adjList := [][]int{
+		{3, 4},
+		{0, 2},
+		{3},
+		{2, 4},
+		{0},
+	}
+	w := bytes.NewBuffer(nil)
+	if err := WriteTo(w, adjList); err != nil {
+		t.Error(err)
+	}
+	expected := "5\n2 3 4\n2 0 2\n1 3\n2 2 4\n1 0\n"
+	if expected != w.String() {
+		t.Logf("expected: %s\n", expected)
+		t.Errorf("found: %s\n", w.String())
+	}
+}
+
+func ExampleReadFrom() {
+	r := strings.NewReader("5\n2 3 4\n2 0 2\n1 3\n2 2 4\n1 0\n")
+	var adjList [][]int
+	if err := ReadFrom(r, &adjList); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(adjList)
+	// Output:
+	// [[3 4] [0 2] [3] [2 4] [0]]
+}

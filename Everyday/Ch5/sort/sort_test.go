c7e9bcf862ebb19f3e614d3a8b9b808095523326
DoLim <limdongyoung0@gmail.com>
2020-02-29 10:05:43 +0000

    Studied golang sort

---
+++ Everyday/Ch5/sort/sort_test.go
@@ -0,0 +1,32 @@
+package sort_test
+
+import "strings"
+
+import "sort"
+
+import "fmt"
+
+type CaseInsensitive []string
+
+func (c CaseInsensitive) Len() int {
+	return len(c)
+}
+
+func (c CaseInsensitive) Less(i, j int) bool {
+	return strings.ToLower(c[i]) < strings.ToLower(c[j]) ||
+		(strings.ToLower(c[i]) == strings.ToLower(c[j]) && c[i] < c[j])
+}
+
+func (c CaseInsensitive) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func ExampleCaseInsensitive_sort() {
+	apple := CaseInsensitive([]string {
+		"iPhone", "iPad", "MacBook", "AppStore",
+	})
+	sort.Sort(apple)
+	fmt.Println(apple)
+	// Output:
+	// [AppStore iPad iPhone MacBook]
+}
